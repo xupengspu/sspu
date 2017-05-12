3f1f3a3f7fb665a43862251c3aac396877b589ad
xupeng <xupeng@xgift.org>
2017-05-12 17:25:12 +0800

    s

--- Application/Home/Controller/PageController.class.php
+++ Application/Home/Controller/PageController.class.php
@@ -19,19 +19,27 @@ class PageController extends Controller
      */
     public function content()
     {
+        $sid = I("sid");
         $menus = MenuBiz::search(I('menu_name'));
-        $sid = I('sid');
+        $banner = M("banner")->find();
+        $menu = M("menu")->where("id='$sid'")->find();
+
+        $this->assign('banner', $banner['path']);
+        $this->assign('menus', $menus);
+        $this->assign('title', $menu['menu_name']);
 
         //查询文章
-        $sql = "";
-
-//        $menu
-        $banner = M("banner")->find();
-        $this->assign('banner', $banner['path']);
+        $sql = "select a.title,a.id,a.content ,a.create_time from menu_article ma , article a where ma.article_id=a.id and ma.menu_id='" . I('sid') . "' order by a.create_time desc";
+        $result = M()->query($sql);
 
 
-        $this->assign('menus', $menus);
-        $this->display('/contet');
+        if (sizeof($result) > 1) {
+            $this->assign('contents', $result);
+            $this->display('/contet-list');
+        } else {
+            $this->assign('content', $result[0]);
+            $this->display('/contet');
+        }
     }
 
     /**
@@ -45,5 +53,23 @@ class PageController extends Controller
         $menus = MenuBiz::search(I('menu_name'));
         $this->assign('menus', $menus);
         $this->display('/contet-list');
+    }
+
+    /**
+     * 查看详情
+     */
+    public function detail()
+    {
+        $menus = MenuBiz::search();
+        $banner = M("banner")->find();
+        $article_id = I("aid");
+
+        $this->assign('banner', $banner['path']);
+        $this->assign('menus', $menus);
+
+        $result = M("article")->where("id='$article_id'")->find();
+        $this->assign('content', $result);
+        $this->assign('title', $result['title']);
+        $this->display('/contet');
     }
 }
